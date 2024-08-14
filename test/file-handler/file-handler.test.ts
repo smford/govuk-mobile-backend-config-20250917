f363c727d792f79e7d9b01cae915b4079448cb35
James Miller <james.miller1@digital.cabinet-office.gov.uk>
2024-08-14 10:59:55 +0100

    update to randomly generate versions in file tests

--- test/file-handler/file-handler.test.ts
+++ test/file-handler/file-handler.test.ts
@@ -2,58 +2,75 @@ import {FileHandler} from '../../src/file-handler';
 
 const fileHandler = new FileHandler();
 
+function randomSemver(): String {
+  const version = Array(3).fill(0);
+  version.forEach((_, i) => {
+    version[i] = (Math.random() * 10) | 0;
+  });
+  return version.join('.');
+}
+
 describe('FileHandler#extractVersionFromFilename', () => {
   it('extracts a basic version number from a filename', () => {
-    const filename = '/path/to/file/7.6.1.yaml';
+    const version = randomSemver();
+    const filename = `/path/to/file/${version}.yaml`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts a basic version number from a filename with a yml extension', () => {
-    const filename = '/path/to/file/7.6.1.yml';
+    const version = randomSemver();
+    const filename = `/path/to/file/${version}.yml`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts a version number with additional details from a filename', () => {
-    const filename = '/path/to/file/7.6.1+rc2.yaml';
+    const version = randomSemver();
+    const filename = `/path/to/file/${version}+rc2.yaml`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1+rc2');
+    expect(res).toEqual(`${version}+rc2`);
   });
 
   it('extracts a version number even with no file extension', () => {
-    const filename = '/path/to/file/7.6.1';
+    const version = randomSemver();
+    const filename = `/path/to/file/${version}`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts version number from a relative path', () => {
-    const filename = './files/7.6.1.yaml';
+    const version = randomSemver();
+    const filename = `./files/${version}.yaml`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts version number from a relative path with no extension', () => {
-    const filename = './files/7.6.1';
+    const version = randomSemver();
+    const filename = `./files/${version}`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts version number from a filename with no path', () => {
-    const filename = '7.6.1.yaml';
+    const version = randomSemver();
+    const filename = `${version}.yaml`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts version number from a filename with no path and no extension', () => {
-    const filename = '7.6.1';
+    const version = randomSemver();
+    const filename = `${version}`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1');
+    expect(res).toEqual(version);
   });
 
   it('extracts complex version number from a filename with no path', () => {
-    const filename = '7.6.1+rc3.yaml';
+    const version = randomSemver();
+    const filename = `${version}+rc3.yaml`;
     const res = fileHandler.extractVersionFromFilename(filename);
-    expect(res).toEqual('7.6.1+rc3');
+    expect(res).toEqual(`${version}+rc3`);
   });
 });
